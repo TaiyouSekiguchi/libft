935812398eadd35388349f0dd751ad22a34f5718
tsekiguc <tsekiguc@student.42tokyo.jp>
2022-05-25 08:47:57 +0900

    rm int i

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: tsekiguc <tsekiguc@student.42tokyo.jp      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/14 19:04:32 by tsekiguc          #+#    #+#             */
-/*   Updated: 2021/07/14 19:14:32 by tsekiguc         ###   ########.fr       */
+/*   Updated: 2022/05/25 08:47:31 by tsekiguc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,11 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	unsigned char	*ucdst;
 	unsigned char	*ucsrc;
-	size_t			i;
 
 	if (len == 0 || dst == src)
 		return (dst);
 	ucdst = (unsigned char *)dst;
 	ucsrc = (unsigned char *)src;
-	i = 0;
 	if (ucdst > ucsrc)
 	{
 		while (len--)
